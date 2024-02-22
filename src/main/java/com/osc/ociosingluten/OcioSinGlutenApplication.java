e2fd042c04eb34ee401e22bda555889c653beb76
Álvaro Ordóñez Romero <aor00039@red.ujaen.es>
2024-02-22 19:31:31 +0100

    Commit de configuración

---
+++ src/main/java/com/osc/ociosingluten/OcioSinGlutenApplication.java
@@ -0,0 +1,13 @@
+package com.osc.ociosingluten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcioSinGlutenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcioSinGlutenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/osc/ociosingluten/OcioSinGlutenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osc.ociosingluten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcioSinGlutenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
